bd5e0b057ff955387399468764f98c75543a6782
Maryam Nadali <nadali.mary@gmail.com>
2025-08-13 12:30:16 +0330

    Update args.py

--- args.py
+++ args.py
@@ -59,7 +59,7 @@ def make_args():
 
     #anchor selection
     parser.add_argument('--anchor_method', dest='anchor_method', default='random', type=str,
-                    choices=['random', 'fixed4', 'degree', 'degree_coverage', 'enhanced_degree_coverage', 'degree_farthest', 'betweenness', 'eigenvector'],
+                    choices=['random', 'degree', 'degree_coverage', 'enhanced_degree_coverage', 'degree_farthest', 'betweenness', 'eigenvector'],
                     help='anchor selection method')
     #agg_s in paper
     parser.add_argument('--aggregation', dest='aggregation', default='mean', type=str,
