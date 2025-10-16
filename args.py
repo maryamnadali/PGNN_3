753f0bf27eea2e40cb4572ef62ff9b6dc6fcc80e
Maryam Nadali <nadali.mary@gmail.com>
2025-10-16 18:38:24 +0330

    Update args.py

--- args.py
+++ args.py
@@ -74,6 +74,7 @@ def make_args():
                     help='c in c*ln(N) and c*ln(M) sampling for ProbSparse')
     parser.add_argument('--prob_min_top', dest='prob_min_top', default=1, type=int,
                     help='minimum selected queries (nodes) and sampled anchors per node')
+    #!python main.py --model PGNN --dataset communities --comb_mode probxattn --prob_context_mode mean
     parser.add_argument('--prob_context_mode', dest='prob_context_mode', default='concat', type=str, choices=['concat', 'mean'],
                     help='How to form initial context in ProbSparse: concat (default) or mean (Informer-style)')
 
