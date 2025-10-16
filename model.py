5f0b09fda272259ac3161c52f8f3101570e4932a
Maryam Nadali <nadali.mary@gmail.com>
2025-10-16 16:46:35 +0330

    Update model.py

--- model.py
+++ model.py
@@ -12,7 +12,8 @@ import math
 
 # # PGNN layer, only pick closest node for message passing
 class PGNN_layer(nn.Module):
-    def __init__(self, input_dim, output_dim, dist_trainable=True, aggregation='mean', comb_mode='concat', num_heads=4, prob_factor=5, prob_min_top=1):
+    def __init__(self, input_dim, output_dim, dist_trainable=True, aggregation='mean', comb_mode='concat', 
+                 num_heads=4, prob_factor=5, prob_min_top=1, prob_context_mode='concat'):
         super(PGNN_layer, self).__init__()
         self.input_dim = input_dim
         self.dist_trainable = dist_trainable
