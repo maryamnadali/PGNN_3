0f8459c6e0e2aa1e3efaa4902e913a3f5de18038
Maryam Nadali <nadali.mary@gmail.com>
2025-12-11 11:51:52 +0330

    Update main.py

--- main.py
+++ main.py
@@ -41,9 +41,11 @@ if __name__ == '__main__':
         args.task = task  # مقدار آرگومان رو آپدیت کن که همه جا درست کار کنه
         if args.dataset=='All':
             if task == 'link':
-                datasets_name = ['grid','communities','ppi', 'Amazon-Computers','Amazon-Photo','Flickr', 'PubMed', 'karate', 'Wiki','BlogCatalog','Facebook']
+                datasets_name = ['grid','communities','ppi', 'Amazon-Computers','Amazon-Photo','Flickr',
+                                 'PubMed', 'karate', 'Wiki','BlogCatalog','Facebook','Minesweeper','Tolokers']
             else:
-                datasets_name = ['communities', 'email', 'protein', 'Amazon-Computers','Amazon-Photo','Flickr', 'PubMed', 'karate', 'Wiki','BlogCatalog','Facebook']
+                datasets_name = ['communities', 'email', 'protein', 'Amazon-Computers','Amazon-Photo','Flickr',
+                                 'PubMed', 'karate', 'Wiki','BlogCatalog','Facebook','Minesweeper','Tolokers']
         else:
             datasets_name = [args.dataset]
         print(f"\n🔁 Now running TASK: {task.upper()} on dataset(s): {datasets_name}\n")
