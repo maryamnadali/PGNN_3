f3e1e9303624ebf037effc03493d5b8a01208698
Maryam Nadali <nadali.mary@gmail.com>
2026-08-15 17:59:17 +0330

    Update utils.py

--- utils.py
+++ utils.py
@@ -7,6 +7,20 @@ import torch.nn.functional as F
 
 
 
+def set_seed(seed):
+    seed = int(seed)
+
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
 
 # # approximate
 def get_edge_mask_link_negative_approximate(mask_link_positive, num_nodes, num_negtive_edges):
